9c3257543d8ee527032912d8c7ad93c602ecfae3
Stephan Schuler <stephan.schuler@netlogix.de>
2024-02-22 17:17:46 +0100

    Add stencil

---
+++ dist/probetag/my-component.entry.js
@@ -0,0 +1,27 @@
+import { r as registerInstance, h } from './index-2ae4abe3.js';
+
+function format(first, middle, last) {
+    return (first || '') + (middle ? ` ${middle}` : '') + (last ? ` ${last}` : '');
+}
+
+const myComponentCss = ":host{display:block}";
+
+const MyComponent = class {
+    constructor(hostRef) {
+        registerInstance(this, hostRef);
+        this.first = undefined;
+        this.middle = undefined;
+        this.last = undefined;
+    }
+    getText() {
+        return format(this.first, this.middle, this.last);
+    }
+    render() {
+        return h("div", { key: '70654fc83039b5f659efe99cc990e9b86d20dbe0' }, "Hello, World! I'm ", this.getText());
+    }
+};
+MyComponent.style = myComponentCss;
+
+export { MyComponent as my_component };
+
+//# sourceMappingURL=my-component.entry.js.map
